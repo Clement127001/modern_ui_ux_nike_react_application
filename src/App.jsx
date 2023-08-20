2d06e1745dcab05c0d02ced10c2a08e400d1bc71
Clement127001 <babiyonclement18@gmail.com>
2023-08-20 11:31:22 +0530

    add new assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,62 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl text-emerald-500 font-semibold">Hello world!</h1>
-  );
-}
+import React from "react";
+
+import Nav from "./Components/Nav";
+
+import {
+  CustomerReview,
+  Footer,
+  Hero,
+  PopularProducts,
+  Subscribe,
+  SuperQuality,
+  Servies,
+  SpecialOffer,
+} from "./Section";
+
+const App = () => (
+  <main className="realtive">
+    <Nav />
+    {/* hero section */}
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+
+    {/* popular products */}
+    <section className="padding">
+      <PopularProducts />
+    </section>
+
+    {/* super quality */}
+    <section className="padding">
+      <SuperQuality />
+    </section>
+
+    {/* Service */}
+    <section className="padding-x py-10">
+      <Servies />
+    </section>
+
+    {/* Special offer */}
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+
+    {/* Customer reviews */}
+
+    <section className="padding bg-pale-blue">
+      <CustomerReview />
+    </section>
+
+    {/* news letter */}
+    <section className="padding py-16 sm:py-32 w-full ">
+      <Subscribe />
+    </section>
+
+    {/* footer */}
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ src/Section/index.js
@@ -0,0 +1,19 @@
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Servies from "./Servies";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Footer,
+  CustomerReview,
+  PopularProducts,
+  Servies,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
