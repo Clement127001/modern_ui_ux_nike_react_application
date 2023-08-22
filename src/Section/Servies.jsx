42f4456153996628ad6231dcf104d6526549ed09
Clement127001 <babiyonclement18@gmail.com>
2023-08-22 21:50:55 +0530

    update styles

--- src/Section/Servies.jsx
+++ src/Section/Servies.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { services } from "../Constants";
+import ServiceCard from "../Components/ServiceCard";
 
 const Servies = () => {
-  return <div>Servies</div>;
+  return (
+    <div className="flex flex-1 max-container w-full  gap-12 justify-center flex-wrap">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </div>
+  );
 };
 
 export default Servies;
