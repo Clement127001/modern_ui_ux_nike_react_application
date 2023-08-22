98bd134808ebb76f3dfa02776b92bb387f32e771
Clement127001 <babiyonclement18@gmail.com>
2023-08-22 22:26:29 +0530

    add dynamic style

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Button = ({ label, imgURl }) => {
+const Button = ({ label, imgURL, bgColor, textColor, borderColor }) => {
   return (
     <a
       href="#products"
-      className=" flex justigy-between items-center gap-3 mt-6 outline-coral-red bg-coral-red rounded-full text-white px-4 py-2  cursor-pointer "
+      className={` flex justigy-between items-center gap-3 mt-6  border-2 ${
+        bgColor
+          ? `${bgColor} ${textColor} ${borderColor}`
+          : `border-coral-red bg-coral-red   text-white`
+      } rounded-full px-4 py-2  cursor-pointer `}
     >
       <div>{label}</div>
 
-      {imgURl && <img src={imgURl} alt="arrow right" />}
+      {imgURL && <img src={imgURL} alt="arrow right" />}
     </a>
   );
 };
